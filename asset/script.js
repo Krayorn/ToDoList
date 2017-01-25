bd567abe5a20df424766e63864fe70861fbc1ce6
cheikhomarsow <cheikhomar60@gmail.com>
2017-01-26 00:33:16 +0100

    ne pas prendre en compte

--- asset/script.js
+++ asset/script.js
@@ -4,7 +4,12 @@ window.onload =  function(){
     var addColumnButton = document.getElementById('add_column');
 
     function newColumn (){
-        columnArea.innerHTML += '<div class="column"><input type="text" name="title"/><a href="#" id="addTask">Nouveau Post-It</a><a id="deleteColumn" href="#">Delete</a></div>';
+        columnArea.innerHTML += '<div class="column">' +
+            '                           <div class="title_column"><h3>Titre</h3></div>' +
+            '                           <input type="text" name="title"/>' +
+            '                           <a href="#" id="addTask"><img src="img/add_task.png" alt="add_task"/></a>' +
+            '                           <a id="deleteColumn" href="#">Delete</a>' +
+            '                   </div>';
     }
     function newTask(){
         this.innerHTML += '<div class="task"></div>'
