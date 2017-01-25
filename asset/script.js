10934c0ca8db2c594b7b2d4bb51ce730ca51cfe8
Nathael Arki <nathael.arki@supinternet.fr>
2017-01-25 22:24:36 +0100

    cabug

--- asset/script.js
+++ asset/script.js
@@ -1,13 +1,20 @@
 window.onload =  function(){
     var columnArea = document.getElementById('container');
+    var column = document.getElementsByClassName('column');
 
     var addColumnButton = document.getElementById('add_column');
+    var addPostitButton = document.getElementById('addTask');
 
     function newColumn (){
         columnArea.innerHTML += '<div class="column"><input type="text" name="title"/><a href="#" id="addTask">Nouveau Post-It</a><a id="deleteColumn" href="#">Delete</a></div>';
     }
     function newTask(){
-        this.innerHTML += '<div class="task"></div>'
+        columnArea.innerHTML += '<div class="task">Salut</div>';
+        console.log('yo');
+    }
+
+    addPostitButton.onclick = function(){
+        newTask();
     }
 
     addColumnButton.onclick = function(){
