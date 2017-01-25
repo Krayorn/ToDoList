3b0a1f4a5f90890649d3077112074b5254b5fa5b
Nathael Arki <nathael.arki@supinternet.fr>
2017-01-25 12:50:11 +0100

    script add column

---
+++ asset/script.js
@@ -0,0 +1,18 @@
+window.onload =  function(){
+
+    var columnArea = document.getElementById('container');
+
+    var addColumnButton = document.getElementById('add_column');
+
+    function newColumn (){
+        columnArea.innerHTLM += '<div class="column"></div>';
+    }
+    function newTask(){
+
+    }
+
+    addColumnButton.onclick = function(){
+        newColumn();
+    }
+
+}
