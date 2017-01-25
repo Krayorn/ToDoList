c4eb2dd17f790c3bffe2eafb4fa0f5695deafbbe
Nathael Arki <nathael.arki@supinternet.fr>
2017-01-25 15:55:56 +0100

    Column done

--- asset/script.js
+++ asset/script.js
@@ -4,10 +4,10 @@ window.onload =  function(){
     var addColumnButton = document.getElementById('add_column');
 
     function newColumn (){
-        columnArea.innerHTML += '<div class="column">TEST</div>';
+        columnArea.innerHTML += '<div class="column"><input type="text" name="title"/><a href="#" id="addTask">Nouveau Post-It</a><a id="deleteColumn" href="#">Delete</a></div>';
     }
     function newTask(){
-
+        this.innerHTML += '<div class="task"></div>'
     }
 
     addColumnButton.onclick = function(){
