9755ef4db6e57d81cbee3673e79b6072f844edd9
cheikhomarsow <cheikhomar60@gmail.com>
2017-01-27 14:02:31 +0100

    ...

--- asset/script.js
+++ asset/script.js
@@ -1,8 +1,6 @@
 window.onload =  function(){
     var columnArea = document.querySelector('#container');
-
     var addColumnButton = document.querySelector('#add_column');
-    var addPostitButton = document.querySelectorAll('.addTask');
 
     function addTask(currentDiv){
         var d=document.createElement("div");
@@ -12,22 +10,58 @@ window.onload =  function(){
 
     function addColumn(){
         columnArea.innerHTML += '<div class="column">' +
-            '<input type="text" name="title"/>' +
-            '<a href="#" class="addTask"><img class="icon" src="../asset/img/add_task.png"/></a>' +
-            '<a href="#" class="deleteColumn" href="#">' +
-            '<img class="icon icon_border_left" src="../asset/img/remove_task.png"/>' +
-            '</a>' +
+            '<span class="titlecolumn">Title</span>' +
+            '<input class="hide_input" type="text" name="title"/><input class="hide_button" type="submit"/>' +
+            '<a class="addTask"><img class="icon" src="../asset/img/add_task.png"/></a>' +
+            '<a class="deleteColumn"><img class="icon icon_border_left" src="../asset/img/remove_task.png"/></a>' +
             '</div>';
+        addPostitButton = document.querySelectorAll('.addTask');
+        deleteColumnButton = document.querySelectorAll('.deleteColumn');
+        columntitle = document.querySelectorAll('.titlecolumn');
+        refresh_for();
+    }
+
+    function deleteColumn(currentDiv){
+        columnArea.removeChild(currentDiv);
+    }
+
+    function changetitle(currentDiv){
+        element = currentDiv ;
+        var myInput = element.childNodes[1];
+        var myButton = element.childNodes[2];
+        myInput.style.display= 'inline';
+        myButton.style.display= 'inline';
+        myButton.onclick = function(){
+            if (myInput.value.length == ''){
+                element.childNodes[0].innerHTML = 'Title';
+            }
+            else{
+                element.childNodes[0].innerHTML = myInput.value;
+            }
+            myInput.style.display= 'none';
+            myButton.style.display= 'none';
+        }
     }
 
     addColumnButton.onclick = function(){
         addColumn();
     }
 
-
-    for(var i = 0; i < addPostitButton.length; i++){
-        addPostitButton[i].onclick = function () {
-            addTask(this.parentElement);
+    function refresh_for(){
+        for(var i = 0; i < addPostitButton.length; i++){
+            addPostitButton[i].onclick = function () {
+                addTask(this.parentElement);
+            }
+        }
+        for(var i = 0; i < deleteColumnButton.length; i++){
+            deleteColumnButton[i].onclick = function(){
+                deleteColumn(this.parentElement);
+            }
+        }
+        for(var i = 0; i < columntitle.length; i++){
+            columntitle[i].onclick = function(){
+                changetitle(this.parentElement);
+            }
         }
     }
 };
